7fcd2592a5f86ec773bd10cfe999bcc93c432f99
Chris Hiestand <chrishiestand@gmail.com>
2016-08-06 02:48:04 -0700

    add checkAllDomainsP(), update jsdoc
    re-ordered some functions for docs

--- src/index.js
+++ src/index.js
@@ -11,50 +11,19 @@ if (!process.env.FASTLY_API_KEY) {
 **/
 
 /**
- * send the fastly request
- * @param  {Object} request options
- * @return {Promise} resolving to response
+ * Instant Purge a particular service of items tagged with a Surrogate Key. Soft Purging sets an object's TTL to 0s, forcing revalidation. For best results, Soft Purging should be used in conjuction with stale_while_revalidate and stale_if_error.
+ * @param  {string}  serviceId
+ * @param  {string}  key
+ * @param  {Boolean} [soft=false] sets an object's TTL to 0s
+ * @return {Promise} resolves to parsed api result object
  */
-export function sendP({baseUrl, endpoint = '', form, headers = {}, method = 'GET', timeout = 5000}) {
+export function purgeP(serviceId, key, soft = false) {
 
-  baseUrl = baseUrl || 'https://api.fastly.com'
+  const method   = 'POST'
+  const endpoint = `/service/${serviceId}/purge/${key}`
+  const headers  = soft ? {'Fastly-Soft-Purge': 1} : {}
 
-  const fastlyKey = process.env.FASTLY_API_KEY
-  const uri       = `${baseUrl}/${endpoint}`
-
-  if (uri === '/') {
-    throw new Error('missing baseUrl and/or endpoint!')
-  }
-
-  const defaultHeaders = {
-    'Fastly-Key': fastlyKey,
-    'Accept': 'application/json'
-  }
-
-  let sendHeaders = ramda.merge(defaultHeaders, headers)
-
-  const options = {
-    method,
-    uri,
-    headers: sendHeaders,
-    timeout,
-  }
-
-  if (form) {
-    options.form = form
-  }
-
-  return request(options)
-  .then((response) => {
-
-    if (!response) {
-      let optionsLog = options
-      delete optionsLog.headers['Fastly-Key']
-      throw new Error(`request failed with options: ${JSON.stringify(optionsLog)}`)
-    }
-
-    return JSON.parse(response)
-  })
+  return sendP({endpoint, method, headers})
 }
 
 /**
@@ -69,22 +38,6 @@ export function purgeUrlP(url, soft = false) {
   const headers = soft ? {'Fastly-Soft-Purge': 1} : {}
 
   return sendP({baseUrl: url, method, headers})
-}
-
-/**
- * Instant Purge a particular service of items tagged with a Surrogate Key. Soft Purging sets an object's TTL to 0s, forcing revalidation. For best results, Soft Purging should be used in conjuction with stale_while_revalidate and stale_if_error.
- * @param  {string}  serviceId
- * @param  {string}  key
- * @param  {Boolean} [soft=false] sets an object's TTL to 0s
- * @return {Promise} resolves to parsed api result object
- */
-export function purgeP(serviceId, key, soft = false) {
-
-  const method   = 'POST'
-  const endpoint = `/service/${serviceId}/purge/${key}`
-  const headers  = soft ? {'Fastly-Soft-Purge': 1} : {}
-
-  return sendP({endpoint, method, headers})
 }
 
 /**
@@ -181,10 +134,9 @@ export function updateServiceVersionP(serviceId, version, data) {
  */
 export function validateServiceVersionP(serviceId, version) {
 
-  const method   = 'GET'
   const endpoint = `/service/${serviceId}/version/${version}/validate`
 
-  return sendP({method, endpoint})
+  return sendP({endpoint})
 }
 
 /**
@@ -277,10 +229,9 @@ export function renameServiceP(serviceId, newName) {
  */
 export function ListServicesP() {
 
-  const method   = 'GET'
   const endpoint = '/service'
 
-  return sendP({method, endpoint})
+  return sendP({endpoint})
 }
 
 /**
@@ -290,10 +241,9 @@ export function ListServicesP() {
  */
 export function getServiceP(serviceId) {
 
-  const method   = 'GET'
   const endpoint = `/service/${serviceId}`
 
-  return sendP({method, endpoint})
+  return sendP({endpoint})
 }
 
 /**
@@ -303,10 +253,9 @@ export function getServiceP(serviceId) {
  */
 export function getServiceDetailsP(serviceId) {
 
-  const method   = 'GET'
   const endpoint = `/service/${serviceId}/details`
 
-  return sendP({method, endpoint})
+  return sendP({endpoint})
 }
 
 /**
@@ -316,10 +265,9 @@ export function getServiceDetailsP(serviceId) {
  */
 export function getServiceDomainsP(id) {
 
-  const method   = 'GET'
   const endpoint = `/service/${id}/domain`
 
-  return sendP({method, endpoint})
+  return sendP({endpoint})
 }
 
 /**
@@ -335,6 +283,19 @@ export function createDomainP(serviceId, version, data) {
   const endpoint = `/service/${serviceId}/version/${version}/domain`
 
   return sendP({method, endpoint, form: data})
+}
+
+/**
+ * Check all domains' DNS for a particular service and version
+ * @param  {string} serviceId
+ * @param  {number} version
+ * @return {Promise} resolves to parsed api result object
+ */
+export function checkAllDomainsP(serviceId, version) {
+
+  const endpoint = `/service/${serviceId}/version/${version}/domain/check_all`
+
+  return sendP({endpoint})
 }
 
 /**
@@ -365,4 +326,51 @@ export function updateSettingsP(serviceId, version, settings) {
   const endpoint = `/service/${serviceId}/version/${version}/settings`
 
   return sendP({method, endpoint, form: settings})
+}
+
+/**
+ * Wrapper to send a fastly api request. Not for external use unless you want to send a custom request.
+ * @param  {Object} request options: baseUrl (string), form (object), endpoint (string), headers (object), method (string), timeout (number)
+ * @return {Promise} resolving to response
+ */
+export function sendP({baseUrl, form, endpoint = '', headers = {}, method = 'GET', timeout = 5000}) {
+
+  baseUrl = baseUrl || 'https://api.fastly.com'
+
+  const fastlyKey = process.env.FASTLY_API_KEY
+  const uri       = `${baseUrl}/${endpoint}`
+
+  if (uri === '/') {
+    throw new Error('missing baseUrl and/or endpoint!')
+  }
+
+  const defaultHeaders = {
+    'Fastly-Key': fastlyKey,
+    'Accept': 'application/json'
+  }
+
+  let sendHeaders = ramda.merge(defaultHeaders, headers)
+
+  const options = {
+    method,
+    uri,
+    headers: sendHeaders,
+    timeout,
+  }
+
+  if (form) {
+    options.form = form
+  }
+
+  return request(options)
+  .then((response) => {
+
+    if (!response) {
+      let optionsLog = options
+      delete optionsLog.headers['Fastly-Key']
+      throw new Error(`request failed with options: ${JSON.stringify(optionsLog)}`)
+    }
+
+    return JSON.parse(response)
+  })
 }
